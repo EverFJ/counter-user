103a5f02548f272f22122bab4fa8c059144455db
Francois Janson <jansonfrancois@gmail.com>
2021-08-17 15:28:52 +0200

    Implementing logic and styling boxes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import React from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import UserInfo from "./components/UserInfo";
+import usersJson from "./users.json";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
   render() {
     return (
       <div>
-        <p>HELLO</p>
+        <div className="counter">
+          <Counter
+            count={this.state.count}
+            increment={() => this.setState({ count: this.state.count + 1 })}
+            substract={
+              this.state.count !== 0
+                ? () => this.setState({ count: this.state.count - 1 })
+                : console.log("Do nothing")
+            }
+          />
+        </div>
+        {usersJson
+          .filter((elem, index) => index < this.state.count)
+          .map((elem, index) => {
+            return (
+              <UserInfo
+                name={elem.name}
+                email={elem.email}
+                website={elem.website}
+              />
+            );
+          })}
       </div>
     );
   }
